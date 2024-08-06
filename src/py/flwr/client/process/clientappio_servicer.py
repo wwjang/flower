9d15661b4d46c2528a4bf17142cb1e3d98d9d9bb
Chong Shen Ng <chong.shen@flower.ai>
2024-08-06 11:42:34 +0100

    Add servicer

---
+++ src/py/flwr/client/process/clientappio_servicer.py
@@ -0,0 +1,77 @@
+# Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""ClientAppIo API servicer."""
+
+
+import grpc
+
+from flwr.common import Context, Message
+from flwr.common.typing import Code, Run, Status  # TODO: add Fab type
+from flwr.proto import appio_pb2_grpc  # pylint: disable=E0611
+from flwr.proto.appio_pb2 import (  # pylint: disable=E0611
+    PullClientAppInputsRequest,
+    PullClientAppInputsResponse,
+    PushClientAppOutputsRequest,
+    PushClientAppOutputsResponse,
+)
+
+
+class ClientAppIoServicer(appio_pb2_grpc.ClientAppIoServicer):
+    """ClientAppIo API servicer."""
+
+    def __init__(self) -> None:
+        self.message: Message = None
+        self.context: Context = None
+        self.fab = None
+        self.run: Run = None
+        self.token: int = None
+
+    def PullClientAppInputs(
+        self, request: PullClientAppInputsRequest, context: grpc.ServicerContext
+    ) -> PullClientAppInputsResponse:
+        assert request.token == self.token
+        return PullClientAppInputsResponse(
+            message=self.message,
+            context=self.context,
+            fab=self.fab,
+            run=self.run,
+        )
+
+    def PushClientAppOutputs(
+        self, request: PushClientAppOutputsRequest, context: grpc.ServicerContext
+    ) -> PushClientAppOutputsResponse:
+        assert request.token == self.token
+        # Update Message and Context
+        self.message = request.message
+        self.context = request.context
+
+        code = Code.OK
+        message = "OK"
+        status = Status(code=code, message=message)
+        return PushClientAppOutputsResponse(status=status)
+
+    def set_object(  # pylint: disable=R0913
+        self, message: Message, context: Context, fab, run: Run, token: int
+    ) -> None:
+        """Set client app objects."""
+        self.message = message
+        self.context = context
+        self.fab = fab
+        self.run = run
+        self.token = token
+
+    def get_object(self) -> tuple[Message, Context]:
+        """Get client app objects."""
+        return self.message, self.context
---
+++ src/py/flwr/client/process/process.py
@@ -0,0 +1,99 @@
+# Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Flower background ClientApp."""
+
+from logging import DEBUG, ERROR, INFO
+
+import grpc
+
+from flwr.cli.install import install_from_fab
+from flwr.client.client_app import ClientApp
+from flwr.client.supernode.app import _get_load_client_app_fn
+from flwr.common.grpc import GRPC_MAX_MESSAGE_LENGTH, create_channel
+from flwr.common.logger import log
+
+from .clientappio_servicer import PullClie
+
+
+def _run_background_client(
+    address: str,
+    token: str,
+) -> None:
+    """Run background Flower ClientApp process."""
+
+    def on_channel_state_change(channel_connectivity: str) -> None:
+        """Log channel connectivity."""
+        log(DEBUG, channel_connectivity)
+
+    channel = create_channel(
+        server_address=address,
+        insecure=True,
+    )
+    channel.subscribe(on_channel_state_change)
+
+    try:
+        stub = ClientAppIoStub(channel)
+
+        req = PullClientAppInputsRequest(token=token)
+        res = stub.PullClientAppInputs(req)
+
+        fab_file = res.fab  # Seems unnecessary?
+        run = res.run
+
+        install_from_fab(
+            fab_file, None, True
+        )  # Ensures FAB is installed (default is Flower directory)
+        load_client_app_fn = _get_load_client_app_fn(
+            default_app_ref="",
+            project_dir="",
+            multi_app=True,
+            flwr_dir=None,
+        )
+        client_app: ClientApp = load_client_app_fn(
+            run.fab_id, run.fab_version  # Can be optimized later
+        )
+        # Execute ClientApp
+        reply_message, reply_context = client_app(
+            message=res.message, context=res.context
+        )
+
+        req = PushClientAppOutputsRequests(
+            token=token,
+            message=reply_message,
+            context=reply_context,
+        )
+        res = stub.PushClientAppOutputs(req)
+    except KeyboardInterrupt:
+        log(INFO, "Closing connection")
+    except grpc.RpcError as e:
+        log(ERROR, "GRPC error occurred: %s", str(e))
+    finally:
+        channel.close()
+
+
+def run_clientappio_api_grpc(
+    address: str = "0.0.0.0:9094",
+):
+    """Run ClientAppIo API (gRPC-rere)."""
+    clientappio_servicer: grpc.Server = ClientAppIoServicer()
+    clientappio_add_servicer_to_server_fn = add_ClientAppIoServicer_to_server
+    clientappio_grpc_server = generic_create_grpc_server(
+        server_and_add_fn=(clientappio_servicer, clientappio_add_servicer_to_server_fn),
+        server_address=address,
+        max_message_length=GRPC_MAX_MESSAGE_LENGTH,
+    )
+    log(INFO, "Starting Flower ClientAppIo gRPC server on %s", address)
+    clientappio_grpc_server.start()
+    return clientappio_servicer, clientappio_grpc_server
