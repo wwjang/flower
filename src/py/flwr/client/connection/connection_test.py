28ef987dc1b4c6d58a3982ae7c3e57d9d18cb374
Heng Pan <pan@flower.ai>
2024-08-22 16:20:06 +0100

    complete ConnectionTest and fix tiny bugs

--- src/py/flwr/client/connection/connection_test.py
+++ src/py/flwr/client/connection/connection_test.py
@@ -29,6 +29,7 @@ from flwr.common import Message, serde
 from flwr.common.retry_invoker import RetryInvoker, exponential
 from flwr.common.serde_test import RecordMaker
 from flwr.common.typing import Fab, Run
+from flwr.proto.node_pb2 import Node  # pylint: disable=E0611
 
 from . import Connection, GrpcRereConnection
 
@@ -73,16 +74,20 @@ class ConnectionTest(unittest.TestCase):
     def setUp(self) -> None:
         """Prepare before each test."""
         mk = RecordMaker()
+        node_id = 6
+        run_id = 616
         received_msg = Message(mk.metadata(), mk.recordset(1, 1, 1))
+        received_msg.metadata.__dict__["_src_node_id"] = 0
+        received_msg.metadata.dst_node_id = node_id  # Target at this node
         sent_msg = received_msg.create_reply(mk.recordset(1, 1, 1))
         run_info = Run(616, "dummy/mock", "v0.0", "#mock hash", {})
         fab = Fab("#mock hash", b"mock fab content")
         self.pairs = IoPairs(
-            create_node=((), 6),
+            create_node=((), node_id),
             delete_node=((), None),
             receive=((), received_msg),
             send=((sent_msg,), None),
-            get_run=((616,), run_info),
+            get_run=((run_id,), run_info),
             get_fab=(("#mock hash",), fab),
         )
         self.start_stub_patcher()
@@ -109,6 +114,61 @@ class ConnectionTest(unittest.TestCase):
         # Assert
         self.assertEqual(node_id, expected_node_id)
 
+    def test_delete_node(self) -> None:
+        """Test delete_node method."""
+        # Execute
+        self.conn.create_node()
+        self.conn.delete_node()
+
+    def test_receive(self) -> None:
+        """Test receive method."""
+        # Prepare
+        expected_msg: Message = self.pairs.receive[1]
+
+        # Execute
+        self.conn.create_node()
+        actual_msg = self.conn.receive()
+
+        # Assert
+        assert actual_msg is not None
+        # Message object doesn't support `==` operator
+        self.assertEqual(actual_msg.metadata, expected_msg.metadata)
+        self.assertEqual(actual_msg.content, expected_msg.content)
+
+    def test_send(self) -> None:
+        """Test send method."""
+        # Prepare
+        msg: Message = self.pairs.send[0][0]
+
+        # Execute
+        self.conn.create_node()
+        self.conn.receive()
+        self.conn.send(msg)
+
+    def test_get_run(self) -> None:
+        """Test get_run method."""
+        # Prepare
+        (run_id,), run_info = self.pairs.get_run
+
+        # Execute
+        self.conn.create_node()
+        actual_run_info = self.conn.get_run(run_id)
+
+        # Assert
+        self.assertEqual(actual_run_info, run_info)
+
+    def test_get_fab(self) -> None:
+        """Test get_fab method."""
+        # Prepare
+        (fab_hash,), fab = self.pairs.get_fab
+
+        # Execute
+        self.conn.create_node()
+        actual_fab = self.conn.get_fab(fab_hash)
+
+        # Assert
+        self.assertEqual(actual_fab, fab)
+
 
 class GrpcRereConnectionTest(ConnectionTest):
     """Tests for GrpcRereConnection."""
@@ -129,24 +189,25 @@ class GrpcRereConnectionTest(ConnectionTest):
 
         # Mock CreateNode
         _, expected_nid = self.pairs.create_node
-        stub.CreateNode.return_value = Mock(node=Mock(node_id=expected_nid))
+        stub.CreateNode.return_value = Mock(node=Node(node_id=expected_nid))
 
         # Mock DeleteNode
-        def delete_node_side_effect(req: Any) -> Any:
-            self.assertEqual(req.node.node_id, expected_nid)
+        def delete_node_side_effect(request: Any) -> Any:
+            self.assertEqual(request.node.node_id, expected_nid)
 
         stub.DeleteNode.side_effect = delete_node_side_effect
 
         # Mock PullTaskIns (for `receive` method)
         _, received_msg = self.pairs.receive
         task_ins = serde.message_to_taskins(received_msg)
-        stub.PullTaskIns.return_value = task_ins
+        task_ins.task_id = received_msg.metadata.message_id
+        stub.PullTaskIns.return_value = Mock(task_ins_list=[task_ins])
 
         # Mock PushTaskRes (for `send` method)
         (sent_msg,), _ = self.pairs.send
 
-        def send_side_effect(req: Any) -> Any:
-            self.assertEqual(req.task_res_list[0], task_res)
+        def send_side_effect(request: Any) -> Any:
+            self.assertEqual(request.task_res_list[0], task_res)
 
         task_res = serde.message_to_taskres(sent_msg)
         stub.PushTaskRes.side_effect = send_side_effect
@@ -154,8 +215,8 @@ class GrpcRereConnectionTest(ConnectionTest):
         # Mock GetRun
         (run_id,), run_info = self.pairs.get_run
 
-        def get_run_side_effect(req: Any) -> Any:
-            self.assertEqual(req.run_id, run_id)
+        def get_run_side_effect(request: Any) -> Any:
+            self.assertEqual(request.run_id, run_id)
             return Mock(run=run_info)
 
         stub.GetRun.side_effect = get_run_side_effect
@@ -163,8 +224,8 @@ class GrpcRereConnectionTest(ConnectionTest):
         # Mock GetFab
         (fab_hash,), fab = self.pairs.get_fab
 
-        def get_fab_side_effect(req: Any) -> Any:
-            self.assertEqual(req.hash_str, fab_hash)
+        def get_fab_side_effect(request: Any) -> Any:
+            self.assertEqual(request.hash_str, fab_hash)
             return Mock(fab=fab)
 
         stub.GetFab.side_effect = get_fab_side_effect
